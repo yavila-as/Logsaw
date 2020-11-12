1ed43fc87f8d169bb354596bf66e8c134901254f
yerlin-avila <avantica-yerlin.avila@mibanco.com.pe>
2020-11-12 16:58:40 -0600

    Winner posibilities

--- zoovu-game/src/containers/GameScreen/index.jsx
+++ zoovu-game/src/containers/GameScreen/index.jsx
@@ -8,56 +8,66 @@ import imageu from '../../images/zoovuu.svg';
 import boxImage from '../../images/greenBox.png';
 import './styles.css';
 
-
+const initialState = {
+  list: [
+      { number: 1, image: `${imageZ}`},
+      { number: 2, image: `${imageo}`},
+      { number: 2, image: `${imageo}`},
+      { number: 3, image: `${imagev}`},
+      { number: 4, image: `${imageu}`},
+  ],
+  editableList: [
+      { number: 5, image: `${boxImage}`},
+      { number: 6, image: `${boxImage}`},
+      { number: 7, image: `${boxImage}`},
+      { number: 8, image: `${boxImage}`},
+      { number: 9, image: `${boxImage}`},
+  ],
+  shuffledList: [],
+  seconds: 0,
+  gameStarted: false,
+  win: false,
+};
 
 export default class GameScreen extends Component {
-    componentDidMount(){
-        this.shufflePieces();
-        
-    }
+  constructor(props) {
+    super(props)
+    this.state = initialState;
+  }
 
-    shufflePieces = () =>{
-        const shuffled = this.state.list;
-    
-        for (let i = shuffled.length - 1; i > 0; i--) {
-          let j = Math.floor(Math.random() * (i + 1));
-          let tmp = shuffled[i];
-          shuffled[i] = shuffled[j];
-          shuffled[j] = tmp;
-        }
-    
-        this.setState({list:shuffled});
-    }
+  componentDidMount(){
+    this.shufflePieces();
+  }
+  
+  shufflePieces = () =>{
+    const shuffled = this.state.list;
 
-  state = {
-      list: [
-        { number: 0, image: `${imageZ}`},
-        { number: 1, image: `${imageo}`},
-        { number: 2, image: `${imageo}`},
-        { number: 3, image: `${imagev}`},
-        { number: 4, image: `${imageu}`},
-      ],
-    editableList: [
-        { number: 5, image: `${boxImage}`},
-        { number: 6, image: `${boxImage}`},
-        { number: 7, image: `${boxImage}`},
-        { number: 8, image: `${boxImage}`},
-        { number: 9, image: `${boxImage}`},
-    ],
-    seconds: 0,
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let tmp = shuffled[i];
+      shuffled[i] = shuffled[j];
+      shuffled[j] = tmp;
+    }
+    this.setState({shuffledList: shuffled});
+  }
+
+  timeCounter = (time) => {
+    this.setState({
+      seconds : this.state.seconds + time
+    })
   }
 
   onDragStart = (event) => {
-      console.log('sdd');
-    setInterval(() => {
-        this.setState({
-          seconds : this.state.seconds+1
-        })
+    if (!this.state.gameStarted) {
+      setInterval(() => {
+        this.timeCounter(1);
       }, 1000)
+      this.setState({ gameStarted: true });
+    } 
     const initialPosition = Number(event.currentTarget.dataset.position);
     event.dataTransfer.setData("draggedFrom", initialPosition);
   }
-
+  
   onDragOver = (event) => {
     event.preventDefault();
   }
@@ -65,7 +75,7 @@ export default class GameScreen extends Component {
   onDrop = (event) => {
     let draggedFrom = event.dataTransfer.getData("draggedFrom");
     let draggedTo = Number(event.currentTarget.dataset.position);
-    const itemDragged = this.state.list[draggedFrom];
+    const itemDragged = this.state.shuffledList[draggedFrom];
     const remainingItems = this.state.editableList.filter((item, index) => index !== Number(draggedTo));
 
     let editableList = [
@@ -73,71 +83,93 @@ export default class GameScreen extends Component {
       itemDragged,
       ...remainingItems.slice(draggedTo)
     ];
-console.log(this.state.list[draggedFrom].number,'dede',draggedTo)
-        if(this.state.list[draggedFrom].number !== draggedTo){
-            this.setState({
-            seconds : this.state.seconds+10
-            })
-        }
-    let list = this.state.list.filter((item, index) => index !== Number(draggedFrom));
+    const itemDraggedA = this.state.editableList[draggedTo];
+    const remainingItemsA = this.state.shuffledList.filter((item, index) => index !== Number(draggedFrom));
+
+    let shuffledList = [
+      ...remainingItemsA.slice(0, draggedFrom),
+      itemDraggedA,
+      ...remainingItemsA.slice(draggedFrom)
+    ];
+    if(this.state.shuffledList[draggedFrom].number !== draggedTo){
+      console.log('jhhgghgh');
+      this.setState({
+        seconds : this.state.seconds + 10
+      })
+    }
     this.setState({
       ...this.state,
       editableList,
-      list
+      shuffledList
+    }); 
+    const temp = [1, 2, 2, 3, 4]
+    const listChecked = editableList.map((element, index) => {
+      return element.number === temp[index] ? true : false;
     });
-
-    
+    this.setState({
+      win: !listChecked.includes(false) && !editableList.some(e => e.image === `${boxImage}`),
+    }, () =>{
+      if(this.state.win){
+        setTimeout(()=> this.onUserWin(), 10000);
+      }
+    });
   }
 
+  onUserWin(){
+    alert('nos fuimos');
+    this.setState(initialState);
+    this.shufflePieces();
+    //clearInterval(myVar);
+  }
+
+  
   render() {
-      return (
-            <Box display='flex' flexWrap="wrap">
-                <Grid
-                    container
-                    direction="column"
-                    justify="left"
-                    alignItems="left"
-                >
-                    <Typography>Good luck,{this.props.userName}!</Typography>
-                    <p>
-                        Your score: {this.state.seconds} seconds
-                    </p>);
-                    <Typography>Pick up the right cards</Typography>
-                    <section>
-                            {this.state.list.map( (item, index) => {
-                            return(
-                            <item 
-                            key={index}
-                            data-position={index}
-                            draggable
-                            onDragStart={(event) => this.onDragStart(event)}
-                            onDragOver={(event) => this.onDragOver(event)}
-                            onDrop={(event) => this.onDrop(event)}
-                            >
-                                <img src={item.image} className="imageBox"/>
-                            </item>
-                            )
-                            })}
-                        </section>
-                    <Typography>..and drop them here to make hte logo great again!</Typography>
-                    <section>
-                {this.state.editableList.map( (item, index) => {
-                return(
-                <item 
-                key={index}
-                data-position={index}
-                draggable
-                onDragStart={(event) => this.onDragStart(event)}
-                onDragOver={(event) => this.onDragOver(event)}
-                onDrop={(event) => this.onDrop(event)}
-                >
-                    <img src={item.image} className="borderBox"/>
-                </item>
-                )
-                })}
-            </section>
-                </Grid>
-            </Box>
-        );
-    }
+    return (
+      <Box display='flex' flexWrap="wrap">
+        <Grid
+            container
+            direction="column"
+            justify="left"
+            alignItems="left"
+        >
+          <Typography>Good luck,{this.props.userName}!</Typography>
+          <p>
+              Your score: {this.state.seconds} seconds
+          </p>
+          <Typography>Pick up the right cards</Typography>
+          <section>
+              {this.state.shuffledList.map( (item, index) => (
+                  <item 
+                  key={index}
+                  data-position={index}
+                  draggable
+                  onDragStart={(event) => this.onDragStart(event)}
+                  onDragOver={(event) => this.onDragOver(event)}
+                  onDrop={(event) => this.onDrop(event)}
+                  >
+                      <img src={item.image} className="imageBox"/>
+                  </item>
+              ))}
+              </section>
+          <Typography>..and drop them here to make the logo great again!</Typography>
+          <section>
+              {this.state.editableList.map( (item, index) => {
+              return(
+              <item 
+              key={index}
+              data-position={index}
+              draggable
+              onDragStart={(event) => this.onDragStart(event)}
+              onDragOver={(event) => this.onDragOver(event)}
+              onDrop={(event) => this.onDrop(event)}
+              >
+                  <img src={item.image} className="imageBox"/>
+              </item>
+              )
+              })}
+          </section>
+        </Grid>
+      </Box>
+    );
+  }
 }
